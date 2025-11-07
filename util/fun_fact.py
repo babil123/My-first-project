9410e4b3e3250d8e4bfba3201eae69e0dac2e9cd
Babil <babilbkv2@gmail.com>
2025-11-07 09:38:11 +0530

    Added fun fact script

---
+++ util/fun_fact.py
@@ -0,0 +1,10 @@
+# fun_fact.py
+import random
+
+facts = [
+    "Python was named after Monty Python, not the snake 🐍",
+    "Git was created by Linus Torvalds, the same person who created Linux 💻",
+    "The first computer bug was an actual moth 🪲"
+]
+
+print("💡 Fun Fact:", random.choice(facts))
