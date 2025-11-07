758e26d18b734d4aed517364f3effc4a65c20789
Babil <babilbkv2@gmail.com>
2025-11-07 09:35:54 +0530

    added user greeting feature and utils.py helper file

--- util/utils.py
+++ util/utils.py
@@ -1,2 +1,13 @@
-def greet(name):
-    print(f"Hello, {name}! Welcome back.")
+# utils.py
+from datetime import datetime
+
+def greet_user(name):
+    current_hour = datetime.now().hour
+    if current_hour < 12:
+        greeting = "Good morning"
+    elif current_hour < 18:
+        greeting = "Good afternoon"
+    else:
+        greeting = "Good evening"
+    print(f"{greeting}, {name}! Welcome to your first GitHub project 🎉")
+
